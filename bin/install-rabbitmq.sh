aec5e1a69d80d90199f0258819cd77e4ca0c3014
William Berman <wlbberman@gmail.com>
2019-07-29 20:36:44 -0700

    Add the rabbitmq installation bit

--- bin/install-rabbitmq.sh
+++ bin/install-rabbitmq.sh
@@ -1,9 +1,24 @@
 #! /bin/bash
 
+# Installs, runs, and enables rabbitmq
+
+set -e
+
 # Install erlang first
 wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
 
 echo "deb https://packages.erlang-solutions.com/ubuntu bionic contrib" | tee /etc/apt/sources.list.d/rabbitmq.list
 
-apt update
-apt -y install erlang
+apt-get update
+apt-get install -y erlang
+
+# Now install rabbitmq
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
+
+echo "deb https://dl.bintray.com/rabbitmq/debian $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/rabbitmq.list
+apt-get update
+apt-get install -y rabbitmq-server
+
+systemctl start rabbitmq-server
+systemctl enable rabbitmq-server
